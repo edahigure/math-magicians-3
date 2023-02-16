ae74c4b8721f52ec305cd96f409666dcf4077258
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-02-16 11:50:45 -0700

    Finish Calculator using classes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import calculate from '../logic/calculate';
-// eslint-disable import/no-extraneous-dependencies
 
-// eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
     this.state = {
@@ -26,8 +23,6 @@ class Calculator extends React.Component {
       next: obj.next,
       operation: obj.operation,
     }));
-    console.log('buttonName', buttonName);
-    console.log('obj', obj);
   };
 
   render() {
